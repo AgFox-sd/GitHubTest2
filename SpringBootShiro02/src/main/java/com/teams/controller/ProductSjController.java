4f798088c67ed52709ad56782024142d846f1f33
zhu-chao331 <2659166612@qq.com>
2020-04-22 20:16:25 +0800

    update 11111

--- SpringBootShiro02/src/main/java/com/teams/controller/ProductSjController.java
+++ SpringBootShiro02/src/main/java/com/teams/controller/ProductSjController.java
@@ -1,5 +1,10 @@
 package com.teams.controller;
 
 public class ProductSjController {
-	
+  public void wuhu() {
+	  System.out.println("芜湖");
+  }
+  public  void qifei() {
+	System.out.println("起飞");
+}
 }
